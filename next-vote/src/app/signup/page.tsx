e8a95f703ea173269d14855e19e59478e34358b1
jungyungee <jungyungee0610@gmail.com>
2025-11-15 01:50:55 +0900

    #3 [MOD] develop 브랜치 받아온 레이아웃에 맞춰서 수정

--- next-vote/src/app/signup/page.tsx
+++ next-vote/src/app/signup/page.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "../../components/SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center gradient-radial">
+    <div className="min-h-screen flex items-center justify-center pt-20">
       <div className="my-8 px-8 py-6 rounded-2xl shadow-xl bg-white">
         <h1 className="text-head-2-bold mb-4 text-center">회원가입</h1>
         <p className="text-center text-gray-700 mb-6 text-body-2-semibold">투표 시스템에 가입하고 투표에 참여하세요</p>
