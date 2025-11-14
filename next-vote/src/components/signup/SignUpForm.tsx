806c5b7aa4ab593fbe93c0b6b28148aba89b682e
jungyungee <jungyungee0610@gmail.com>
2025-11-15 02:04:18 +0900

    #3 [ADD] 사소한 디자인 수정 및 로그인 링크 연결

--- next-vote/src/components/signup/SignUpForm.tsx
+++ next-vote/src/components/signup/SignUpForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import { teamOptions } from "../constants/teamOptions";
-import { nameOptions } from "../constants/nameOptions";
+import { teamOptions } from "../../constants/teamOptions";
+import { nameOptions } from "../../constants/nameOptions";
 
 const SignUpForm = () => {
   const [selectedTeam, setSelectedTeam] = useState<'FRONT-END' | 'BACK-END' | null>(null);
@@ -35,7 +35,7 @@ const SignUpForm = () => {
       {/* 팀명 */}
       <label className="text-body-2-semibold mb-1 flex">팀명</label>
       <div className="flex flex-col gap-2 mb-6">
-        <select className="border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700">
+        <select className="border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700">
           <option value="">팀을 선택하세요</option>
           {teamOptions.map((team) => (
             <option key={team} value={team}>{team}</option>
@@ -45,7 +45,7 @@ const SignUpForm = () => {
       {/* 이름 */}
       <label className="text-body-2-semibold mb-1 flex">이름</label>
       <div className="flex flex-col gap-2 mb-6">
-        <select className="border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700">
+        <select className="border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700">
           <option value="">이름을 선택하세요</option>
           {nameOptions.map(name => (
             <option key={name} value={name}>{name}</option>
@@ -58,7 +58,7 @@ const SignUpForm = () => {
         <input
           type="text"
           placeholder="아이디를 입력하세요"
-          className="w-2/3 border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold"
+          className="w-2/3 border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold"
           value={userId}
           onChange={e => setUserId(e.target.value)}
         />
@@ -77,7 +77,7 @@ const SignUpForm = () => {
         <input
           type="email"
           placeholder="이메일을 입력하세요"
-          className="w-2/3 border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold"
+          className="w-2/3 border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold"
           value={userEmail}
           onChange={e => setUserEmail(e.target.value)}
         />
@@ -92,12 +92,12 @@ const SignUpForm = () => {
       </div>
       {/* 비밀번호 */}
       <label className="text-body-2-semibold mb-1 flex">비밀번호</label>
-      <input type="password" placeholder="비밀번호를 입력하세요" className="border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold mb-6" />
+      <input type="password" placeholder="비밀번호를 입력하세요" className="border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold mb-6" />
       {/* 비밀번호 재확인 */}
       <label className="text-body-2-semibold mb-1 flex">비밀번호 재확인</label>
-      <input type="password" placeholder="비밀번호를 다시 입력하세요" className="border border-gray-200 bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold" />
+      <input type="password" placeholder="비밀번호를 다시 입력하세요" className="border border-gray-200 text-body-1-semibold bg-gray-300 rounded-[14px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black/50 placeholder:text-body-1-semibold" />
       {/* 회원가입 버튼 */}
-      <button type="submit" className="bg-blue-500 text-white py-3 rounded-[14px] font-bold mt-14 hover:bg-blue-600 transition cursor-pointer">회원가입하기</button>
+      <button type="submit" className="bg-blue-500 text-white text-body-1-medium py-3 rounded-[14px] font-bold mt-14 hover:bg-blue-600 transition cursor-pointer">회원가입하기</button>
     </form>
   );
 };
